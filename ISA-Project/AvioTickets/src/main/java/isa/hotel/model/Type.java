dd050293b4213e06eb49b94ecbb651cd610c0a82
Jelena Matkovic <matkovic.jelena@uns.ac.rs>
2018-12-22 12:51:18 +0100

    izbaceni nepotrebni includovi

--- ISA-Project/AvioTickets/src/main/java/isa/hotel/model/Type.java
+++ ISA-Project/AvioTickets/src/main/java/isa/hotel/model/Type.java
@@ -1,7 +1,5 @@
 package isa.hotel.model;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Check;
 
 import lombok.Data;
 
